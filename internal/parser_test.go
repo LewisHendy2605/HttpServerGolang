be6826883b1d4d2152247fad4968ff6cd816e9a0
Lewis Hendy <lhendy@mclays.co.uk>
2026-05-01 17:03:56 +0100

    added table tests

--- internal/parser_test.go
+++ internal/parser_test.go
@@ -1,32 +1,67 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
 	"testing"
 )
 
+type HttpVersionTestTable struct {
+	name    string
+	input   []byte
+	wantErr bool
+}
+
+func TestParseHttpVersion(t *testing.T) {
+	tests := []HttpVersionTestTable{
+		{name: "valid", input: []byte("HTTP/1.1"), wantErr: false},
+		{name: "missing slash", input: []byte("HTTP1.1"), wantErr: true},
+		{name: "missing period", input: []byte("HTTP/11"), wantErr: true},
+		{name: "missing minor", input: []byte("HTTP/1."), wantErr: true},
+		{name: "missing major", input: []byte("HTTP/.1"), wantErr: true},
+		{name: "missing prefix", input: []byte("/.1"), wantErr: true},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := ParseHttpVersion(tt.input)
+			if (err != nil) != tt.wantErr {
+				t.Fatalf("got err=%v, wantErr=%v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
 func TestParseRequestLine(t *testing.T) {
-	method := []byte("GET")
-	uri := []byte("http://www.w3.org/pub/WWW/TheProject.html")
-	major := []byte("1")
-	minor := []byte("1")
-	version := fmt.Appendf(nil, "HTTP/%s.%s", major, minor)
-
-	requestLine := fmt.Appendf(nil, "%s %s %s", method, uri, version)
-
-	parsed := ParseRequestLine(requestLine)
-
-	if !bytes.Equal(parsed.Method, method) {
-		t.Fatalf("unexpected http method. expected: %s, got %s", method, parsed.Method)
+	tests := []HttpVersionTestTable{
+		{name: "valid", input: []byte("GET http://www.w3.org/pub/WWW/TheProject.html HTTP/1.1"), wantErr: false},
+		{name: "missing method", input: []byte("http://www.w3.org/pub/WWW/TheProject.html HTTP/1.1"), wantErr: true},
+		{name: "missing uri", input: []byte("GET HTTP/1.1"), wantErr: true},
+		{name: "missing version", input: []byte("GET http://www.w3.org/pub/WWW/TheProject.html"), wantErr: true},
 	}
-	if !bytes.Equal(parsed.RequestTarget, uri) {
-		t.Fatalf("unexpected http method. expected: %s, got %s", uri, parsed.RequestTarget)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := ParseRequestLine(tt.input)
+			if (err != nil) != tt.wantErr {
+				t.Fatalf("got err=%v, wantErr=%v", err, tt.wantErr)
+			}
+		})
 	}
-	if !bytes.Equal(parsed.HttpVersion.Major, major) {
-		t.Fatalf("unexpected http method. expected: %s, got %v", major, parsed.HttpVersion.Major)
+
+}
+
+func TestParseFieldLine(t *testing.T) {
+	tests := []HttpVersionTestTable{
+		{name: "valid", input: []byte("Host: example.com"), wantErr: false},
+		{name: "missing colon", input: []byte("Host example.com"), wantErr: true},
+		{name: "space in name", input: []byte("Ho st: value"), wantErr: true},
 	}
-	if !bytes.Equal(parsed.HttpVersion.Minor, minor) {
-		t.Fatalf("unexpected http method. expected: %s, got %v", minor, parsed.HttpVersion.Minor)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := ParseFieldLine(tt.input)
+			if (err != nil) != tt.wantErr {
+				t.Fatalf("got err=%v, wantErr=%v", err, tt.wantErr)
+			}
+		})
 	}
 }
