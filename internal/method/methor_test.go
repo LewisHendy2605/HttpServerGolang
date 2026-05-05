2095a902879b368d26ec0c0e4ee02399477e8b11
Lewis Hendy <lewishendy26@gmail.com>
2026-05-05 21:50:54 +0100

    added method test

---
+++ internal/method/methor_test.go
@@ -0,0 +1,51 @@
+package method
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestMethod(t *testing.T) {
+	// Test GET
+	require.True(t, IsMethod("GET"))
+
+	// Test HEAD
+	require.True(t, IsMethod("HEAD"))
+
+	// Test POST
+	require.True(t, IsMethod("POST"))
+
+	// Test PUT
+	require.True(t, IsMethod("PUT"))
+
+	// Test PATCH
+	require.True(t, IsMethod("PATCH"))
+
+	// Test DELETE
+	require.True(t, IsMethod("DELETE"))
+
+	// Test OPTIONS
+	require.True(t, IsMethod("OPTIONS"))
+
+	// Test TRACE
+	require.True(t, IsMethod("TRACE"))
+
+	// Test bad method
+	require.False(t, IsMethod("TEST"))
+
+	// Test bad method
+	require.False(t, IsMethod(""))
+
+	// Test bad method
+	require.False(t, IsMethod("get"))
+
+	// Test bad method
+	require.False(t, IsMethod("gEt"))
+
+	// Test bad method
+	require.False(t, IsMethod("GEt"))
+
+	// Test bad method
+	require.False(t, IsMethod("Get"))
+}
