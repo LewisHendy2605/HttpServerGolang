cb962750923ab076d975e78619cc9b5ac509cf39
Lewis Hendy <lewishendy26@gmail.com>
2026-05-14 21:44:12 +0100

    added builder functions to response for use by server

--- internal/response/response.go
+++ internal/response/response.go
@@ -1,15 +1,33 @@
 package response
 
-import "github.com/LewisHendy2605/HttpServerGolang/internal/field_line"
+import (
+	"fmt"
+
+	"github.com/LewisHendy2605/HttpServerGolang/internal/field_line"
+	"github.com/LewisHendy2605/HttpServerGolang/internal/status_line"
+)
 
 type Response struct {
-	StatusLine string
+	StatusLine status_line.StatusLine
 	Headers    *field_line.Headers
 	Body       []byte
 }
 
 func NewResponse() *Response {
 	return &Response{
-		Headers: field_line.NewHeaders(),
+		StatusLine: status_line.NewStatusLine(),
+		Headers:    field_line.NewHeaders(),
 	}
+}
+
+func (r *Response) Ok() {
+	r.StatusLine = status_line.StatusOK
+}
+
+func (r *Response) String() string {
+	return fmt.Sprintf("%s\r\n%s\r\n%s", r.StatusLine.String(), r.Headers.String(), string(r.Body))
+}
+
+func (r *Response) Byte() []byte {
+	return []byte(r.String())
 }
