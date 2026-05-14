8b82faa40d07a56d78af1e60f3585bcf695d7ff3
Lewis Hendy <lewishendy26@gmail.com>
2026-05-14 21:45:46 +0100

    server now handles requests and responds

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,35 +1,53 @@
 package server
 
 import (
-	"log"
+	"fmt"
+	"io"
 	"net"
 
-	"github.com/LewisHendy2605/HttpServerGolang/internal/request"
+	"github.com/LewisHendy2605/HttpServerGolang/internal/response"
 )
 
 type Server struct {
+	closed bool
 }
 
-// Starts tcp server
-func ListenAndServe(address string) {
-	listener, err := net.Listen("tcp", address)
+// Starts a new tcp server
+func Serve(port uint16) (*Server, error) {
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
-		log.Fatal("error", "error", err)
+		return nil, err
 	}
 
+	s := &Server{closed: false}
+	go s.runServer(listener)
+
+	return s, nil
+}
+
+// Accepts new tcp connection and passes to go routine to handle
+func (s *Server) runServer(listener net.Listener) {
 	for {
 		conn, err := listener.Accept()
-		if err != nil {
-			log.Fatalf("error accepting connection: %v", err)
+		if err != nil || s.closed {
+			return
 		}
 
-		_, err = request.RequestFromReader(conn)
-
-		// TODO: What to do here, do we return 400, 500??
-		// the server shouldn't stop if the request is bad
-		// but also we dnt want to leak internal error messages
-		if err != nil {
-			panic(err)
-		}
+		go s.handle(conn)
 	}
+}
+
+// Handles a new tcp connection
+func (s *Server) handle(conn io.ReadWriteCloser) {
+	res := response.NewResponse()
+	res.Headers.Set("Content-Length", "0")
+	res.Ok()
+
+	conn.Write(res.Byte())
+}
+
+// Closes server connection, and stops handling connection
+func (s *Server) Close() error {
+	s.closed = true
+	return nil
 }
---
+++ internal/server/server_test.go
@@ -0,0 +1,28 @@
+package server
+
+import (
+	"net"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestServer(t *testing.T) {
+	client, conn := net.Pipe()
+	defer client.Close()
+	defer conn.Close()
+
+	go func() {
+		server := &Server{}
+		server.handle(conn)
+	}()
+
+	buf := make([]byte, 4096)
+
+	n, err := client.Read(buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	require.Equal(t, "HTTP/1.1 200 OK\r\ncontent-length: 0\r\n\r\n", string(buf[:n]))
+}
